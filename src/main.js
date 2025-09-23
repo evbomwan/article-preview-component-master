97bde450cb0a0c791f76fb46fc3cbee469fe889d
evbomwan <evbomwanc@gmail.com>
2025-09-24 00:08:15 +0100

    code update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './style.css'
+// Getting the elements from the DOM
+const profile = document.getElementById('profile');
+const shareBtn = document.getElementById('share-btn');
+const shareBar = document.getElementById('share-bar');
+const shareBack = document.getElementById('share-back');
+// adding event listener to the share button
+shareBtn.addEventListener('click', () => {
+  profile.classList.add('hidden');
+  shareBar.classList.remove('hidden');
+})
+// adding event listener to the back button
+shareBack.addEventListener('click', () => {
+  shareBar.classList.add('hidden');
+  profile.classList.remove('hidden');
+});
